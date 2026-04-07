e9ccdfdbde19aac19fc4c9ee3e029282262d2479
Thalles Santos <thalless829@gmail.com>
2026-04-07 20:52:40 -0300

    full stack udemy course

---
+++ Introduction to JavaScript ES6/IntermediateJS.js
@@ -0,0 +1,16 @@
+/*100. Control Statement
+
+var firstname = window.prompt("What is your  name?");
+var secondname = window.prompt("What is your  name?");
+
+var porcentage = Math.random() * 100;
+porcentage = Math.floor(porcentage) + 1;
+
+if (porcentage > 70) {
+    window.alert("Your score is " + porcentage + "%" + " You love each other like Kayne loves Kanye")
+} else {
+    window.alert("Your score is " + porcentage + "%")
+}
+
+*/
+
